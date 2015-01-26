07acc7cb4c4533908c0c954aae49510ad8b8a1b4
Dan Dorman <dan.dorman@gmail.com>
2015-01-25 23:14:04 -0700

    Add single-card component

--- src/clump/core.cljs
+++ src/clump/core.cljs
@@ -1,3 +1,23 @@
 (ns clump.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
+
+(defn card [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div
+        #js {:className "card-container"}
+        (dom/div
+          #js {:className "card"}
+          (dom/div
+            #js {:className "face front"}
+            (dom/svg
+              #js {:viewBox "0 0 100 100"}
+              (dom/circle
+                #js {:className "red striped"
+                     :cx 50 :cy 50 :r 45}))))))))
+
+(om/root card
+         nil
+         {:target (.getElementById js/document "app")})
